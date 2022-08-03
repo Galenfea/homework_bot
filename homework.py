f1102b40e051aa12ab2949a5c911c1424b0d928b
Alexander Otarov <90474383+Galenfea@users.noreply.github.com>
2022-08-03 22:22:14 +0400

    Update homework.py

--- homework.py
+++ homework.py
@@ -14,7 +14,7 @@ from exceptions import APIError, EnvError
 from log_config import log_config
 
 load_dotenv()
-# По требованиям pytest в файле должны быть эти переменные
+# По требованиям заказчика в файле должны быть эти переменные
 PRACTICUM_TOKEN = os.getenv('PRACTICUM_TOKEN')
 TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
 TELEGRAM_CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
@@ -32,7 +32,7 @@ HOMEWORK_STATUSES = {
 }
 
 # Настройки логера и его объявление вынесены за пределы main() в связи с
-# требованием логировать то, что происходит внутри функций. В частности
+# требованием заказчика логировать то, что происходит внутри функций. В частности
 # добавить в логи информацию о том, какой именно токен недоступен в функции
 # check_tokens() или ввести страховочный код в send_message().
 # Во избежание странностей и для удобства настройки имя логера задано жёстко.
@@ -76,7 +76,7 @@ def get_api_answer(current_timestamp):
         f'    Параметры запроса: {params}\n'
     )
     try:
-        # Ситуация дисконекта и невозможности преобразования в json закрыта
+        # Закрыта ситуация дисконекта и невозможности преобразования в json
         response = requests.get(ENDPOINT, headers=HEADERS, params=params)
         response_json = response.json()
     except Exception as error:
@@ -103,7 +103,7 @@ def get_api_answer(current_timestamp):
 
 
 def check_response(response):
-    """Функция проверяет:...
+    """Проверь:...
     - есть ли обновление;
     - содержится ли словарь в response;
     - содержится ли в словаре по индексу homeworks список;
@@ -122,15 +122,14 @@ def check_response(response):
 
 
 def parse_status(homework):
-    """Функция извлекает из входящих данных название домашней работы, её статус...
-    и возвращает текстовую строку для последующей отсылки телеграм ботом.
+    """Извлеки из входящих данных название домашней работы, её статус...
+    и возврати текстовую строку для последующей отсылки телеграм ботом.
     """
     if not isinstance(homework, dict):
         raise TypeError('Информация о домашнем задании - не слловарь: '
                         f'{homework}'
                         )
     homework_name = homework.get('homework_name')
-    # При получении значения из словаря через get функция не должна упасть
     homework_status = homework.get('status')
     if homework_status not in HOMEWORK_STATUSES:
         raise KeyError('Получен незадокументированный статус работы: '
@@ -141,33 +140,28 @@ def parse_status(homework):
 
 
 def check_tokens():
-    """Функция проверяет доступность переменных окружения, если нет...
-    хотя бы одной переменной, то возращает False, иначе - True; логирует
+    """Проверь доступность переменных окружения, если нет...
+    хотя бы одной переменной, то возрати False, иначе - True; логируй
     на уровне critical каких именно переменных не хватает.
     """
     # Правильно было бы вынести это в начало файла, и свести все токены
-    # в словарь, а затем брать значения из словаря, но pytest требуют
+    # в словарь, а затем брать значения из словаря, но заказчик требует
     # наличия переменных в которых будут токены.
     TOKENS = {
         'PRACTICUM_TOKEN': PRACTICUM_TOKEN,
         'TELEGRAM_TOKEN': TELEGRAM_TOKEN,
         'TELEGRAM_CHAT_ID': TELEGRAM_CHAT_ID,
     }
-    # Можно перенести логирование в main(), но в ревью вопрос о логировании
-    # конкретной переменной, которой не хватает, был задан именно в теле
-    # функции check_tokens(), поэтому перенесено в функцию.
     [logger.critical(f'Отсутствует переменная окружения: {token_name}')
      for token_name, token in TOKENS.items() if token is None
      ]
-    # Рефакторинг
     return all(TOKENS.values())
 
 
 def main():
-    """Делает запрос к API, проверяет ответы, при наличии обновления —...
-    получает статус работы из обновления и отправляет сообщение в Telegram,
-    повторяет действия спустя RETRY_TIME, логирует работу
-    (настройки логирования) в log_config.py.
+    """Сделай запрос к API, проверь ответы, при наличии обновления —...
+    получи статус работы из обновления и отправь сообщение в Telegram,
+    повтори действия спустя RETRY_TIME, логируй работу в log_config.py.
     """
     logger.debug('Начало работы')
     if not check_tokens():
@@ -186,7 +180,7 @@ def main():
                          f'{chek_time.strftime("%Y-%m-%d %H:%M:%S")}'
                          )
             # Проверка наличия метки времени происходит в main через try:,
-            # поскольку pytest неадекватно реагирует на эту проверку в функции
+            # поскольку заказчик запретил эту проверку в функции
             # check_response
             current_timestamp = response['current_date']
             if len(homeworks) == 0:
